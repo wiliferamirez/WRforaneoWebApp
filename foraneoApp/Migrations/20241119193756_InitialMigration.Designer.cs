1e495d89666587bf6843841ceee9df01150c7f31
William Ramirez <william@Williams-MacBook-Air.local>
2024-11-19 14:38:45 -0500

    add initial migration to mainProject

--- foraneoApp/Migrations/20241119193756_InitialMigration.Designer.cs
+++ foraneoApp/Migrations/20241119193756_InitialMigration.Designer.cs
@@ -2,17 +2,20 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using foraneoApp.Data;
 
 #nullable disable
 
-namespace foraneoApp.Data.Migrations
+namespace foraneoApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241119193756_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");

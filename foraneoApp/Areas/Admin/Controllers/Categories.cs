f9fc3542f9a8d759dbd7c147fccdb30e02cb33e1
William Ramirez <william@Williams-MacBook-Air.local>
2024-11-23 14:29:05 -0500

    create categories controller

--- foraneoApp/Areas/Admin/Controllers/Categories.cs
+++ foraneoApp/Areas/Admin/Controllers/Categories.cs
@@ -1,14 +1,32 @@
+using foraneoApp.DataAccess.Data.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace foraneoApp.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class Categories : Controller
     {
-        // GET: Categories
+        private readonly IWorkContainer _workContainer;
+
+        public Categories(IWorkContainer workContainer)
+        {
+            _workContainer = workContainer;
+        }
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        #region APICalls
+
+        [HttpGet]
+        public ActionResult GetAllCategories()
+        {
+            return Json(new {data = _workContainer.Category.GetAll()});
+        }
+        
+        #endregion
 
     }
 }

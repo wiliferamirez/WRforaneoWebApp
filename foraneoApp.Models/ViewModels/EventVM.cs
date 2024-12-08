46bddcffcad9865df98dc07be478fca497225c0a
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2024-12-07 20:53:22 -0500

    import Rendering package to Model csproj

--- foraneoApp.Models/ViewModels/EventVM.cs
+++ foraneoApp.Models/ViewModels/EventVM.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace foraneoApp.Models.ViewModels;
 
 public class EventVM
 {
+    public Event Event { get; set; }
     
+    public IEnumerable<SelectListItem> CategoryList { get; set; }
 }
